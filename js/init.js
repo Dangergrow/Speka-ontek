0eaebb45d8d714e43217491c00b8fd5aff782699
Dangergrow <138375779+Dangergrow@users.noreply.github.com>
2026-05-28 15:12:09 +0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,14 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     if(typeof ExcelJS==='undefined'||typeof XLSX==='undefined'){
-        document.body.innerHTML='<h1 style="text-align:center;margin-top:100px;">Ошибка загрузки библиотек</h1>';
+        document.body.innerHTML='<h1 style="text-align:center;margin-top:100px;">Ошибка загрузки</h1>';
         return;
     }
     loadAll();
-    applyAllSettings();
-    buildToolbarV1();
+    buildWorkspaces();
     buildSidebarV2();
+    applyAllSettings();
     buildShortcuts();
     bindAllEvents();
     addTable('USD');
-    updateAllHKDisplays();
 });
